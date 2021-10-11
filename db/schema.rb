804622c01c9a04181513f3bb02902f4359d94508
Drew Jennings <andrew_jennings@ymail.com>
2021-10-11 12:39:33 +0100

    fix type button positioning

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_110502) do
+ActiveRecord::Schema.define(version: 2021_10_11_111634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2021_10_09_110502) do
     t.string "entry_no"
     t.string "type_one"
     t.string "type_two"
+    t.integer "hp"
+    t.integer "attack"
+    t.integer "defense"
+    t.integer "specialattack"
+    t.integer "specialdefense"
+    t.integer "speed"
   end
 
 end
